2c99d75b42818609c94ff8ea1e96b17d2433a5bd
Teriton <subochartem2007@mail.ru>
2026-03-13 17:33:08 +0300

    Main layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let count = $state(0);
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit /site goes here/ to read the documentation {count}</p>
+<button onclick={()=>count++}>Click</button>
