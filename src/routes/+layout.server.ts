062e26d9c88cde6e13e36f3a165273ad2b716fed
Teriton <subochartem2007@mail.ru>
2026-03-15 21:34:17 +0300

    Deleted testing info from header

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,37 +2,8 @@ import type { LayoutServerLoad } from './$types';
 import { ArticlesRepo } from '$lib/articles-repo';
 
 export const load: LayoutServerLoad = async () => {
-	// Temporarily hardcoded for testing
-	const articlesTree = {
-		"title": "articles",
-		"articles": [
-			{
-				"title": "main",
-				"articles": [
-					{"title": "Introduction"},
-					{"title": "Глава 1. Почему шпэк тупой"}
-				]
-			},
-			{
-				"title": "test",
-				"articles": [
-					{"title": "Test"},
-					{
-						"title": "TestPage1",
-						"articles": [{"title": "TestArticle1"}]
-					},
-					{
-						"title": "TestPage2",
-						"articles": [{"title": "TestArticle2"}]
-					}
-				]
-			}
-		]
-	};
-
-	// Uncomment to use from backend
-	// const articleRepo = new ArticlesRepo("localhost:8080");
-	// const articlesTree = await articleRepo.getArticlesTree();
+	const articleRepo = new ArticlesRepo("localhost:8080");
+	const articlesTree = await articleRepo.getArticlesTree();
 
 	return {
 		articlesTree
